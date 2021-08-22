c0e4d1e470199ab5a798e5b2deb81b243a72ad98
mprecilio <84741853+mprecilio@users.noreply.github.com>
2021-08-21 23:21:13 -0500

    set url to ec2 endpoint

--- walmart-coding-challenge-react/src/redux/service.ts
+++ walmart-coding-challenge-react/src/redux/service.ts
@@ -1,2 +1,2 @@
-// export const url: string = 'http://3.144.1.64:9001'
-export const url: string = 'http://localhost:9005'
+export const url: string = 'http://3.144.1.64:9001'
+// export const url: string = 'http://localhost:9005'
